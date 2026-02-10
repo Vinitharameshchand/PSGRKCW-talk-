6f681a5aa19d0463fa727dd036ecabd568b87a32
vinitha <vinitharameshchand@gmail.com>
2026-02-10 23:00:24 +0530

    ufff

--- PSGR_Chatbot/backend/app.py
+++ PSGR_Chatbot/backend/app.py
@@ -12,7 +12,9 @@ app.secret_key = os.environ.get("SECRET_KEY", "dev_fallback_key")
 CORS(app, supports_credentials=True, origins=[
     "https://psgrkcw-talk.vercel.app",
     "http://127.0.0.1:8080",
-    "http://localhost:8080"
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
 ])
 
 print("--- BACKEND STARTING ---")
