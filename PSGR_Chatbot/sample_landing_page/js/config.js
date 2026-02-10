e5241a1e051fce91c9471a0c24eaf93cf3b6ca5b
vinitha <vinitharameshchand@gmail.com>
2026-02-10 10:47:07 +0530

    depolyyyy vercel

--- PSGR_Chatbot/sample_landing_page/js/config.js
+++ PSGR_Chatbot/sample_landing_page/js/config.js
@@ -2,5 +2,5 @@ const config = {
     // CHANGE THIS URL AFTER DEPLOYING BACKEND TO RENDER
     // Local: 'http://127.0.0.1:5001'
     // Production Example: 'https://psgr-chatbot-backend.onrender.com'
-    API_BASE_URL: 'http://127.0.0.1:5001'
+    API_BASE_URL: 'https://psgrkcw-sample-chatbot.onrender.com'
 };
